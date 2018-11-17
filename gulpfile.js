b915a8928e0acad76e605a4cde8040afb10f9e13
liuhefa <faheliu@163.com>
2018-11-17 17:00:43 +0800

    gulprjs

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,8 @@
 let gulp = require('gulp'),
   requirejsOptimize = require('gulp-requirejs-optimize')
 
-gulp.task('scripts', function () {
-  return gulp.src('src/js/*.js')
-    .pipe(requirejsOptimize({
-      baseUrl: 'src/js'
-    }))
-    .pipe(gulp.dest('dist'))
-})
+gulp.task('rjs', function () {
+  return gulp.src('src/js/index.js')
+    .pipe(requirejsOptimize())
+    .pipe(gulp.dest('dist'));
+});
